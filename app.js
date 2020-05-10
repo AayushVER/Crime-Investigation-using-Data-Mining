69caf39077d5121747b50f7db52103619a9e3445
baderaharsh <baderaharsh@gmail.com>
2020-05-11 00:37:13 +0530

    Login and register feature done.

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const ejs = require('ejs');
+const bodyParser = require("body-parser");
+const md5 = require("md5");
 
 const app = express();
 
 app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
 
 let resultHistory = [];
-
+let isLoggedIn = false;
+let currentUserId;
 mongoose.connect("mongodb://localhost:27017/ciudm", {useNewUrlParser:true, useUnifiedTopology:true});
 
 const criminalSchema = {
@@ -32,10 +37,60 @@ const criminalSchema = {
   nationality: String
 };
 
+const userSchema = {
+  username : String,
+  password: String
+}
+
 const Criminal = mongoose.model("criminal", criminalSchema);
+const User = mongoose.model("user", userSchema);
 
 app.get("/",function(req,res){
-    res.send("hello");
+    res.render("login", {message: ""});
+});
+
+app.post("/login",function(req,res){
+    let username = req.body.email;
+    let password = md5(req.body.password);
+
+    User.findOne({username: username}, function(err, user){
+      if(user!==null){
+        if(user.username===username&&user.password===password){
+          isLoggedIn = true;
+          currentUserId = user._id;
+          res.send("Logged In");
+        }else{
+          res.render("login", {message: "*Invalid Credentials. Please try again"});
+        }
+      }else{
+        res.render("login", {message: "*Invalid Credentials. Please try again"});
+      }
+    });
+
+});
+
+app.get("/operation", function(req, res){
+  if(isLoggedIn){
+    res.send("You can perform task")
+  }else{
+    res.render("login",{message: "*Please Login before requesting any operation"});
+  }
+});
+
+app.post("/register",function(req,res){
+  let email = req.body.email;
+  let password = md5(req.body.password);
+  const newUser = new User({
+    username: email,
+    password: password
+  });
+  newUser.save(function(err){
+    if(!err){
+      res.send("User Addeded Succesfully");
+    }else{
+      res.send(err);
+    }
+  });
 });
 
 app.get("/match/:sentSuspect", function(req,res){
@@ -46,7 +101,6 @@ app.get("/match/:sentSuspect", function(req,res){
   let itemsProcessed = 0
       Criminal.findOne({_id: req.params.sentSuspect}, function(err, suspect){
       Criminal.find({_id: {$ne: suspectID}},function(err,criminals){
-        if(!err){
           criminals.forEach(function(criminal){
             itemsProcessed = itemsProcessed+1;
             criminalID=criminal._id
@@ -75,6 +129,17 @@ app.get("/match/:sentSuspect", function(req,res){
                   {
                       totalMatches=totalMatches+1;
                   }
+              }
+              if(suspect.typeOfCrime[0]!==null||criminal.typeOfCrime[0]!==null)
+              {
+                  suspect.typeOfCrime.forEach(function(susCrime){
+                    totalFieds=totalFieds+1;
+                      criminal.typeOfCrime.forEach(function(criCrime){
+                        if(susCrime===criCrime){
+                          totalMatches=totalMatches+1;
+                        }
+                      })
+                  })
               }
               if(suspect.details.skinTone!==null||criminal.details.skinTone!==null)
               {
@@ -110,10 +175,6 @@ app.get("/match/:sentSuspect", function(req,res){
                         }
                       })
                   })
-                  // if(suspect.vehicle[0]===criminal.vehicle[0])
-                  // {
-                  //     totalMatches=totalMatches+1;
-                  // }
               }
               if(suspect.nationality!==null||criminal.nationality!==null)
               {
@@ -129,10 +190,6 @@ app.get("/match/:sentSuspect", function(req,res){
             console.log("Total Match Found : " + totalMatches);
             console.log("Percent : " + percent+"%");
           });
-        }
-        else{
-          console.log(err);
-        }
   });
 });
 res.send("Hello");

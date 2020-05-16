b924a75656d5060498b5c8daae187b685363d275
baderaharsh <baderaharsh@gmail.com>
2020-05-16 16:36:01 +0530

    General JavaScript

--- public/searchBar.js
+++ public/searchBar.js
@@ -1,14 +1,9 @@
-// $(".form-check-input").click( function(){
-//     if($(this).attr("value")==="caseId"){
-//       $(".search-bar").attr("placeholder", "Find case by Case ID");
-//     }else if($(this).attr("value")==="caseTitle"){
-//       $(".search-bar").attr("placeholder", "Find case by Case Title");
-//     }
-// })
-
-
 $(".form-check-input").click( function(){
   let value = $(this).attr("id");
   let msg = "Find case by "+value;
-  $(".filter-search-bar").attr("placeholder", msg);
+  if(value==="Case ID"||value==="Case Title"){
+      $(".search-bar").attr("placeholder", msg);
+  } else {
+      $(".filter-search-bar").attr("placeholder", msg);
+  }
 });

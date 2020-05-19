1d743e61cb2c77dc5403332b6a972132b3cb57d7
baderaharsh <baderaharsh@gmail.com>
2020-05-20 00:55:11 +0530

    Home page UI changes

---
+++ public/homeAnimation.js
@@ -0,0 +1,15 @@
+//
+// $(window).bind("load", function() {
+//    $(".dark-text").slideDown("slow");
+// });
+
+$(document).ready(function(){
+  console.log("page ready");
+    $("h2").slideDown(1200);
+    $(".sick").fadeIn(1200);
+});
+
+// $("h2").click(function(){
+//   console.log("page ready");
+//     $("#hell").slideDown();
+// });
